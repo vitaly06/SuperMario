ef641f74f15d54f552eb19cc1465c30a67774d9a
vitaly <vitaly.sadikov1@yandex.ru>
2022-01-04 18:54:50 +0500

    Добавлена анимация

--- player.py
+++ player.py
@@ -13,17 +13,58 @@ class Player(pygame.sprite.Sprite):
         self.rect = pygame.Rect(x, y, 22, 32)  # прямоугольный объект
         self.speed_y = 0  # вертикальная скорость
         self.onGround = False  # находится ли игрок на земле
+        self.image.set_colorkey(pygame.Color(COLOR))  # делаем фон прозрачным
+        #        Анимация движения вправо
+        boltAnim = []
+        for anim in ANIMATION_RIGHT:
+            boltAnim.append((anim, ANIMATION_DELAY))
+        self.boltAnimRight = pyganim.PygAnimation(boltAnim)
+        self.boltAnimRight.play()
+        #        Анимация движения влево
+        boltAnim = []
+        for anim in ANIMATION_LEFT:
+            boltAnim.append((anim, ANIMATION_DELAY))
+        self.boltAnimLeft = pyganim.PygAnimation(boltAnim)
+        self.boltAnimLeft.play()
+
+        self.boltAnimStay = pyganim.PygAnimation(ANIMATION_STAY)
+        self.boltAnimStay.play()
+        self.boltAnimStay.blit(self.image, (0, 0))  # По-умолчанию, стоим
+
+        self.boltAnimJumpLeft = pyganim.PygAnimation(ANIMATION_JUMP_LEFT)
+        self.boltAnimJumpLeft.play()
+
+        self.boltAnimJumpRight = pyganim.PygAnimation(ANIMATION_JUMP_RIGHT)
+        self.boltAnimJumpRight.play()
+
+        self.boltAnimJump = pyganim.PygAnimation(ANIMATION_JUMP)
+        self.boltAnimJump.play()
 
     def update(self, left, right, up, platforms):
         if up:
             if self.onGround:
                 self.speed_y = -JUMP_POWER
+            self.image.fill(pygame.Color(COLOR))
+            self.boltAnimJump.blit(self.image, (0, 0))
         if left:
             self.speed = -SPEED
+            self.image.fill(pygame.Color(COLOR))
+            if up:  # для прыжка влево есть отдельная анимация
+                self.boltAnimJumpLeft.blit(self.image, (0, 0))
+            else:
+                self.boltAnimLeft.blit(self.image, (0, 0))
         if right:
             self.speed = SPEED
+            self.image.fill(pygame.Color(COLOR))
+            if up:
+                self.boltAnimJumpRight.blit(self.image, (0, 0))
+            else:
+                self.boltAnimRight.blit(self.image, (0, 0))
         if not (left or right):
             self.speed = 0
+            if not up:
+                self.image.fill(pygame.Color(COLOR))
+                self.boltAnimStay.blit(self.image, (0, 0))
         if not self.onGround:
             self.speed_y += GRAVITY
         self.onGround = False
